fc0103eb7063c3504abd01aaf0c75766a44ea128
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-10 05:27:40 +0530

    now server running on port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import cors from "cors";
 
 const app = express();
@@ -8,3 +8,14 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://pradeep2165:gsfaxuasnJnyHCy3@cluster0.ek3y1.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
