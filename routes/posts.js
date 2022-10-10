9f6c6969870e3ad461a10dd81351cf5e5efd8e37
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-10 05:57:08 +0530

    post route modified by importing from its controler

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,6 @@
 import express from "express";
-
+import { getPost } from "../controllers/posts.js";
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("this works");
-});
+router.get("/", getPost);
 
 export default router;
