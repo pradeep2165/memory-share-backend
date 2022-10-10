9e66d35ebec2cc54a93ba8f4135997ea351f7714
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-10 05:48:37 +0530

    first test route implemented

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
-
+import postRoutes from "./routes/posts.js";
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 const CONNECTION_URL = "mongodb+srv://pradeep2165:gsfaxuasnJnyHCy3@cluster0.ek3y1.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
