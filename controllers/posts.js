9657ea544d2d6f1025bd2f5d82358effdc59877b
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-11 06:09:56 +0530

    update function implemented

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,15 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
+
 export const getPost = async (req, res) => {
   try {
     const postMessage = await PostMessage.find();
-    res.status(200).json({ postMessage });
+    res.status(200).json(postMessage);
   } catch (error) {
     res.status(401).json({ message: error.message });
   }
 };
+
 export const createPost = async (req, res) => {
   const post = req.body;
   const newPost = new PostMessage(post);
@@ -18,4 +21,17 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
   // res.send("post creation");
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
 };
