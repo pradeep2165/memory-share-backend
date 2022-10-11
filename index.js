63fd30b23fb60d7ff8db4b065552991376eac74e
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-11 08:56:50 +0530

    .dotenv configured

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -10,12 +13,12 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-const CONNECTION_URL = "mongodb+srv://pradeep2165:gsfaxuasnJnyHCy3@cluster0.ek3y1.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL = "mongodb+srv://pradeep2165:gsfaxuasnJnyHCy3@cluster0.ek3y1.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
