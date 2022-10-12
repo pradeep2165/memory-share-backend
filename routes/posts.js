ea25ac21252287e22c6ff9c2da44b9a6de7407ab
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-10-12 23:15:24 +0530

    comment function added

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost, getPostsBySearch, getPostsByCreator } from "../controllers/posts.js";
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost, getPostsBySearch, getPostsByCreator, commentPost } from "../controllers/posts.js";
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
@@ -14,5 +14,6 @@ router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", commentPost);
 
 export default router;
